e5dcd54f2214f6945d65dfd7569dd1e8d90be249
Joshua Nees <joshua.nees@gmail.com>
2022-04-25 15:25:17 -0700

    Add sample data inserts to DDL.sql

--- db_ddl/DDL.sql
+++ db_ddl/DDL.sql
@@ -65,4 +65,54 @@ CREATE TABLE Sightings_Whales (
     ON DELETE CASCADE
 );
 
+INSERT INTO Species (name, description)
+VALUES 
+("Orca", "Black-and-white patterned body."),
+("Gray", "Gray patches and white mottling on dark skin."),
+("Humpback", "Long pectoral fins and a knobbly head."),
+("Minke", "Black to dark gray with a pale chevron on the back behind the head and above the flippers."),
+("Pacific White Sided Dolphin", "Dark gray flippers and dorsal fin. Light gray patches on the sides.");
+
+INSERT INTO Organizations (name, type)
+VALUES
+("Whale Tales", "Non-profit"),
+("University of Washington", "Educational"),
+("Whale Watchers", "Tourism");
+
+INSERT INTO Whales (name, birthyear, is_female, is_transient, species_id)
+VALUES
+("Tahlequah", 1998, 1, 0, 1),
+("Blackberry", 1991, 0, 0, 1),
+("Oreo", 1985, 1, 0, 1),
+("Alki", 1999, 1, 0, 1);
+
+INSERT INTO Researchers (first_name, last_name, email, organization_id)
+VALUES
+("Abner", "Benedicto", "abenedicto1r@washington.edu", 2),
+("Adaline", "Elcott", "aelcott0@whaletails", 1),
+("Farrah", "Crompton", "fcromptonc@whalewatchers.com", 3),
+("Kristel",	"Seaborne", "kseabornek@whailtales.org", 1),
+("Deva", "Quiddington",	"dquiddington3@washington.edu",	2);
+
+INSERT INTO Sightings(datetime, latitude, longitude, researcher_id)
+VALUES
+('2021/3/27 18:19:00', 47.614081, -122.389926, 1),
+('2021/9/29 2:45:00', 47.648136, -122.493726, 3),
+('2021/4/26 7:19:00', 47.739778, -122.458992, 4),
+('2021/3/19 7:39:00', 47.577108, -122.440209, 2),
+('2021/4/30 5:04:00', 47.546325, -122.44628, 5);
+
+INSERT INTO Sightings_Whales(sighting_id, whale_id)
+VALUES
+(1, 1),
+(1, 2),
+(2, 4),
+(3,	1),
+(3,	2),
+(3,	3),
+(4,	3),
+(4,	4),
+(5,	1),
+(5,	3);
+
 SET FOREIGN_KEY_CHECKS=1;
