ae618fccbebc43531000736d3424c02c09692603
Erin Eckerman <erineckerman@gmail.com>
2022-05-12 15:54:10 -0700

    testing db integration

--- routes/api/whales.js
+++ routes/api/whales.js
@@ -1,50 +1,67 @@
 // This router handles all API requests involving CRUD operations to the whales table
-
+require("dotenv").config();
+const { response } = require("express");
 const express = require("express");
+const res = require("express/lib/response");
 const router = express.Router();
+const pg = require("pg");
 
-// TODO: DB integration.
+pg.sslmode = "require";
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect().catch((e) => console.error("connection error", e.stack));
+client.on("error", (err) => console.log(err));
+
 
 // @route GET api/whales
 // @desc Get all records from the whales table
 router.get("/", (req, res) => {
-    dummy_data = [
-        {
-            "whale_id" : 1,
-            "name" : "Tahlequah",
-            "birthyear" : 1998,
-            "is_female" : 1,
-            "is_transient" : 0,
-            "species_id" : 1
-        },
-        {
-            "whale_id" : 2,
-            "name" : "Blackberry",
-            "birthyear" : 1991,
-            "is_female" : 0,
-            "is_transient" : 0,
-            "species_id" : 1
-        },
-        {
-            "whale_id" : 3,
-            "name" : "Oreo",
-            "birthyear" : 1985,
-            "is_female" : 1,
-            "is_transient" : 0,
-            "species_id" : 1
-        },
-        {
-            "whale_id" : 4,
-            "name" : "Alki",
-            "birthyear" : 1999,
-            "is_female" : 1,
-            "is_transient" : 0,
-            "species_id" : 1
-        }
-    ]
+    // dummy_data = [
+    //     {
+    //         "whale_id" : 1,
+    //         "name" : "Tahlequah",
+    //         "birthyear" : 1998,
+    //         "is_female" : 1,
+    //         "is_transient" : 0,
+    //         "species_id" : 1
+    //     },
+    //     {
+    //         "whale_id" : 2,
+    //         "name" : "Blackberry",
+    //         "birthyear" : 1991,
+    //         "is_female" : 0,
+    //         "is_transient" : 0,
+    //         "species_id" : 1
+    //     },
+    //     {
+    //         "whale_id" : 3,
+    //         "name" : "Oreo",
+    //         "birthyear" : 1985,
+    //         "is_female" : 1,
+    //         "is_transient" : 0,
+    //         "species_id" : 1
+    //     },
+    //     {
+    //         "whale_id" : 4,
+    //         "name" : "Alki",
+    //         "birthyear" : 1999,
+    //         "is_female" : 1,
+    //         "is_transient" : 0,
+    //         "species_id" : 1
+    //     }
+    // ]
 
-    res.json(dummy_data);
+    // res.json(dummy_data);
+    SQL = "SELECT * FROM Whales;"
+    return client.query(SQL).then((result) => {
+        console.log(process.env.DATABASE_URL)
+        res.json(result.rows)
+    })
+    .catch((err) => {
+        console.error(err);
+    })
 });
+
+
 
 
 // @route POST api/whales
