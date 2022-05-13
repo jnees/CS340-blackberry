3852bdddcd95507894214a98f8a902e2e1cb78ca
Joshua Nees <joshua.nees@gmail.com>
2022-05-13 13:15:02 -0700

    Add db connection pool as singleton in db_pool.js

--- routes/api/whales.js
+++ routes/api/whales.js
@@ -2,69 +2,21 @@
 
 const express = require("express");
 const router = express.Router();
-
-// Connect to db
-const { Client } = require('pg');
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl:
-    {
-        rejectUnauthorized: false
-    }
-});
-client.connect().catch((e) => console.error("connection error", e.stack));
-client.on("error", (err) => console.log(err));
-
-
-// TODO: DB integration.
+const pool = require("../../db_pool.js")
 
 // @route GET api/whales
 // @desc Get all records from the whales table
 router.get("/", (req, res) => {
-    dummy_data = [
-        {
-            "whale_id" : 1,
-            "name" : "Tahlequah",
-            "birthyear" : 1998,
-            "is_female" : 1,
-            "is_transient" : 0,
-            "species_id" : 1
-        },
-        {
-            "whale_id" : 2,
-            "name" : "Blackberry",
-            "birthyear" : 1991,
-            "is_female" : 0,
-            "is_transient" : 0,
-            "species_id" : 1
-        },
-        {
-            "whale_id" : 3,
-            "name" : "Oreo",
-            "birthyear" : 1985,
-            "is_female" : 1,
-            "is_transient" : 0,
-            "species_id" : 1
-        },
-        {
-            "whale_id" : 4,
-            "name" : "Alki",
-            "birthyear" : 1999,
-            "is_female" : 1,
-            "is_transient" : 0,
-            "species_id" : 1
-        }
-    ]
-
-    // res.json(dummy_data);
     
     let SQL = "SELECT * FROM Whales;"
-    return client.query(SQL).then((result) => {
-        res.json(result.rows);
-    })
-    .catch((err) => {
-        console.error(err);
-    })
+    
+    return pool.query(SQL)
+        .then((result) => {
+            res.json(result.rows)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 });
 
 
@@ -72,6 +24,7 @@ router.get("/", (req, res) => {
 // @desc Insert records into whales
 router.post("/", (req, res) => {
    res.send("This route should handle adding data into the whales table.")
+
 });
 
 // @route PUT api/whales
