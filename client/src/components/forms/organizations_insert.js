7a55351af243d29dfbc29efb02a894367714b2f6
Joshua Nees <joshua.nees@gmail.com>
2022-05-26 18:15:13 -0700

    Add prefilled Orgs insert dropdowns

--- client/src/components/forms/organizations_insert.js
+++ client/src/components/forms/organizations_insert.js
@@ -1,4 +1,4 @@
-import { React, useState} from 'react';
+import { React, useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -10,19 +10,41 @@ const OrganizationsInsertForm = () => {
     
     let navigate = useNavigate();
 
+    // Initialize state
+    const [name, setName] = useState("");
+    const [type, setType] = useState("");
+    const [org_list, setOrgList] = useState([]);
+
+    useEffect(() => {
+      getOrgData();
+    }, [])
+
     const clearState = () => {
         setName("");
         setType("");
     }
 
-    const dataValidation = () => {
-      // TODO: DATA VALIDATION
-    }
+    const getOrgData = async () => {
+      axios({
+          method: "get", url: "/api/organizations",
+      })
+          .then((res) => {
+              setOrgList(res.data);
+          })
+          .catch((err) => {
+              toast.error('Error getting org names', {});
+          })
+  }
 
     const handleSubmit = async (event) => {
       const msg = toast.loading("Adding record...");
-      dataValidation();
       event.preventDefault();
+
+      // Validate whale selected
+      if (name === "") {
+        return toast.update(msg, { render: "Must select an organization!", type: "error", isLoading: false, autoClose: 3000});
+      }
+
       axios({
           method: "post",
           url: "/api/organizations",
@@ -37,9 +59,7 @@ const OrganizationsInsertForm = () => {
           })
     }
 
-    // Initialize state
-    const [name, setName] = useState("");
-    const [type, setType] = useState("");
+
 
     return (
       <div>
@@ -50,15 +70,25 @@ const OrganizationsInsertForm = () => {
           <form>
               <div class="mb-3">
                   <label for="name" class="form-label">Name</label>
-                  <input type="text" class="form-control" id="name" 
-                    value={name} onChange={e => setName(e.target.value)}
-                  />
+                  <select onChange={e => setName(e.target.value)} class="form-control" id="name">
+                        <option></option>
+                        {org_list.map(org =>
+                            <option 
+                                key={org.name} 
+                                value={org.name}
+                            >{org.name}</option>
+                        )}
+                    </select>
               </div>
               <div class="mb-3">
                   <label for="type" class="form-label">Type</label>
-                  <input type="text" class="form-control" id="type" 
-                    value={type} onChange={e => setType(e.target.value)}
-                  />
+                  <select onChange={e => setType(e.target.value)} class="form-control" id="type">
+                        <option></option>
+                        <option>Educational</option>
+                        <option>Tourism</option>
+                        <option>Non-profit</option>
+                        <option>Other</option>
+                    </select>
               </div>
               <button onClick={(e) => {handleSubmit(e)}} 
                 type="submit" class="btn btn-primary">Add record</button>
