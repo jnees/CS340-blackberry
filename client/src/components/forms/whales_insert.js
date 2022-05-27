f8228d5f5b1f4738b43976205e330f67f10a5963
Joshua Nees <joshua.nees@gmail.com>
2022-05-26 20:43:58 -0700

    Add dropdowns and validation to Whale insert form.

--- client/src/components/forms/whales_insert.js
+++ client/src/components/forms/whales_insert.js
@@ -1,4 +1,4 @@
-import { React, useState} from 'react';
+import { React, useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,6 +11,14 @@ const WhalesInsertForm = () => {
 
     let navigate = useNavigate();
 
+    // Initialize state
+    const [name, setName] = useState("");
+    const [birthyear, setBirthyear] = useState("");
+    const [is_female, setGender] = useState("0");
+    const [is_transient, setTransient] = useState("0");
+    const [species_id, setSpecies] = useState("");
+    const [species_list, setSpeciesList] = useState([]);
+
     const clearState = () => {
         setName("");
         setBirthyear("");
@@ -18,10 +26,52 @@ const WhalesInsertForm = () => {
         setTransient("");
         setSpecies("");
     }
+
+    useEffect(() => {
+        getSpeciesList();
+    }, [])
+
+    const getSpeciesList = async () => {
+        axios({
+            method: "get", url: "/api/species",
+        })
+            .then((res) => {
+                setSpeciesList(res.data);
+            })
+            .catch((err) => {
+                toast.error('Error getting species names', {});
+            })
+    }   
     
     const handleSubmit = async (event) => {
         const msg = toast.loading("Adding record...");
         event.preventDefault();
+
+        // Validate Name
+        if (name === "") {
+            return toast.update(msg, { render: "Must enter a valid name!", type: "error", isLoading: false, autoClose: 3000});
+        }
+
+        // Validate Year
+        if (birthyear === "" || birthyear < 0) {
+            return toast.update(msg, { render: "Must enter a valid birthyear!", type: "error", isLoading: false, autoClose: 3000});
+        }
+
+        // Validate Gender
+        if (is_female === "") {
+            return toast.update(msg, { render: "Must make a selection for is_female!", type: "error", isLoading: false, autoClose: 3000});
+        }
+
+        // Validate Transient
+        if (is_transient === "") {
+            return toast.update(msg, { render: "Must make a selection for is_transient!", type: "error", isLoading: false, autoClose: 3000});
+        }
+
+        // Validate Species ID
+        if (species_id === "") {
+            return toast.update(msg, { render: "Must make a selection for species!", type: "error", isLoading: false, autoClose: 3000});
+        }
+
         axios({
             method: "post",
             url: "/api/whales",
@@ -35,13 +85,6 @@ const WhalesInsertForm = () => {
                 toast.update(msg, { render: "Something went wrong!", type: "error", isLoading: false, autoClose: 3000})
             })
     }
-
-    // Initialize state
-    const [name, setName] = useState("");
-    const [birthyear, setBirthyear] = useState("");
-    const [is_female, setGender] = useState("0");
-    const [is_transient, setTransient] = useState("0");
-    const [species_id, setSpecies] = useState("");
     
       return (
         <div>
@@ -78,8 +121,15 @@ const WhalesInsertForm = () => {
                 </div>
                 <div class="mb-3">
                     <label for="species_id" class="form-label">Species ID</label>
-                    <input type="number" class="form-control" id="species_id" 
-                        value={species_id} onChange={e => setSpecies(e.target.value)}/>
+                    <select onChange={e => setSpecies(e.target.value)} class="form-control" id="species_id">
+                        <option></option>
+                        {species_list.map(species =>
+                            <option 
+                                key={species.species_id} 
+                                value={species.species_id}
+                            >{species.species_id + "- " + species.name}</option>
+                        )}
+                    </select>
                 </div>
                 <button onClick={(e) => {handleSubmit(e)}} type="submit" class="btn btn-primary">Add record</button>
             </form>
